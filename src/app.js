df1c06398501e3c4e7c5e76dcd63bb234eddfb0b
Pavlo <p.bazyliuk@gmail.com>
2017-08-12 20:16:57 +0300

    added event emmiter but not working

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import { RouterConfiguration, Router } from "aurelia-router";
+import { DataRepository } from "services/dataRepository";
+import { inject } from "aurelia-framework";
+import { EventAggregator } from "aurelia-event-aggregator";
 
+@inject(DataRepository, EventAggregator)
 export class App {
+  constructor(dataRepository, eventAggregator) {
+    this.dataRepository = dataRepository;
+    this.eventAggregator = eventAggregator;
+  }
+
+  activate() {
+    this.dataRepository.getPages().then(data => {
+      this.pages = data;
+      console.log(this.pages);
+
+      this.eventAggregator.publish("new-message", this.pages);
+    });
+  }
   configureRouter(config, router) {
     config.options.pushState = true;
     this.router = router;
-    console.log(this.router);
-    config.title = "Aurelia";
     config.map([
       { route: ["", "/pages/:id"], name: "pages", moduleId: "pages/pages" }
     ]);
--- src/pages/pages.js
+++ src/pages/pages.js
@@ -1,27 +1,31 @@
 import { DataRepository } from "../services/dataRepository";
 import { inject } from "aurelia-framework";
 import { Router } from "aurelia-router";
+import { EventAggregator } from "aurelia-event-aggregator";
 
-@inject(DataRepository, Router)
+@inject(DataRepository, Router, EventAggregator)
 export class Pages {
-  constructor(dataRepository, router) {
+  constructor(dataRepository, router, eventAggregator) {
+    this.eventAggregator = eventAggregator;
     this.router = router;
     this.dataRepository = dataRepository;
+    this.pages = [];
   }
 
   activate() {
-    this.dataRepository.getPages().then(data => {
-      this.pages = data;
-      if (
-        this.pages.length > 0 &&
-        this.router.currentInstruction.fragment === "/"
-      ) {
-        console.log(this.router.currentInstruction.fragment);
-        this.router.navigate(`pages/${this.pages[0].pageId}`);
-        this.entity = this.pages[0].content;
-      } else {
-        console.log(this.router.currentInstruction.fragment);
-      }
+    this.eventAggregator.subscribe("new-message", data => {
+      console.log(data);
     });
+
+    //   if (
+    //     this.pages.length > 0 &&
+    //     this.router.currentInstruction.fragment === "/"
+    //   ) {
+    //     console.log(this.router.currentInstruction.fragment);
+    //     this.router.navigate(`pages/${this.pages[0].pageId}`);
+    //     this.entity = this.pages[0].content;
+    //   } else {
+    //     console.log(this.router.currentInstruction.fragment);
+    //   }
   }
 }
